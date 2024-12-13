04df77545e050e09ee0c9eaf833d474637e9d593
Anatolii <chemyl.inc@gmail.com>
2024-12-13 17:53:57 +0600

    add openAI call

--- src/api_handler/call_request.rs
+++ src/api_handler/call_request.rs
@@ -1,16 +1,33 @@
-use crate::models::general::llm::{Message};
+use crate::models::general::llm::Message;
 
 use dotenv::dotenv;
+use reqwest::header::{HeaderMap, HeaderValue};
 use std::env;
+// backend_hive
 
-use reqwest::Client;
-
-
-//call LLM GPT
+// Call LLM GPT
 pub async fn call_gpt(message: Vec<Message>) {
     dotenv().ok();
 
-    //extract api keys
-    let api_keys: String = env::var("GPT_API_KEY").expect("GPT_API_KEY");
+    // Extract api keys
+    let api_key: String = env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found");
+    let api_org: String = env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found");
 
+    // Confirm endpoint
+    let url: &str = "https://api.openai.com/v1/chat/completions";
+
+    // Create headers
+    let mut headers = HeaderMap::new();
+
+    // Create api key headers
+    headers.insert(
+        "authorization",
+        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
+    );
+
+    // Create OpenAI org Header
+    headers.insert(
+        "OpenAI-Organization",
+        HeaderValue::from_str(&api_org.as_str()).unwrap(),
+    );
 }
