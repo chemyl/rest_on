43d6aad428474b095c454354e92b145f582637bb
Anatolii <chemyl.inc@gmail.com>
2024-12-14 15:41:31 +0600

    add ChatCompletion model

--- src/models/general/llm.rs
+++ src/models/general/llm.rs
@@ -3,5 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Message {
     pub role: String,
-    pub content: String,
+    pub content: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ChatCompletion {
+    pub model: String,
+    pub messages: Vec<Message>,
+    pub temperature: f32
 }
