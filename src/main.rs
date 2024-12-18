48a1e459c850bb8aaac816dcf9dbdba27153ab88
Anatolii <chemyl.inc@gmail.com>
2024-12-18 12:16:19 +0600

    cargo fmt code style

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod ai_functions;
 mod api_handler;
-mod models;
 mod helpers;
+mod models;
 
 use helpers::command_lines::get_user_response;
-
 
 fn main() {
     let user_req: String = get_user_response("What are we building today?");
